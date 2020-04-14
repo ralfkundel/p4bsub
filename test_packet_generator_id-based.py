1ace226394d8b8069b483b44dcd5103f4416ce7e
Christoph Gärtner <2522420+c-gaertner@users.noreply.github.com>
2020-04-14 17:33:40 +0200

    Added Scapy packet testing generators

--- test_packet_generator_id-based.py
+++ test_packet_generator_id-based.py
@@ -1,71 +1,52 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from scapy.all import *
 from scapy.layers.l2 import Ether
 
 
-class EventHeader(Packet):
-    name = "EventHeader "
-    fields_desc = [
-        ByteField("mask", 0)
-    ]
-
-
 class Event(Packet):
     name = "EventValue "
     fields_desc = [
+        XByteField("id", 0),
         IntField("value", 0)
     ]
 
-class EventBuilder:
-    mask = set()
+
+class EventEnd(Packet):
+    name = "EventEnded "
+    fields_desc = [
+        XByteField("id", 0),
+    ]
+
+
+class EventBuilderID:
     values = {}
 
-    __maxValues = 8
-
     def add(self, aid, value):
-        if aid < self.__maxValues and aid >= 0:
-            self.mask.add(aid)
-            self.values[aid] = value
+        self.values[aid] = value
 
     def build(self):
-        def mask_to_int():
-            s = ""
-            for i in range(self.__maxValues):
-                if i in self.mask:
-                    s += "1"
-                else:
-                    s += "0"
-
-            print("Bitmask: " + s + " " + str(hex(int(s, 2))))
-            print()
-            return int(s, 2)
-
         included = list(self.values.keys())
-        included.sort()
+        # included.sort()
 
         if len(included) == 0:
             raise Exception("You need to specify at least one value.")
 
-        ret = Ether(dst="ff:ff:ff:ff:ff:ff", type=0x9001) / EventHeader(mask=mask_to_int())
+        ret = Ether(dst="ff:ff:ff:ff:ff:ff", type=0x9001)
 
-        for index, aid in enumerate(included):
-            # if index+1 >= len(included):
-            #     next = 0
-            # else:
-            #     next = included[index+1]
+        for aid in included:
+            ret = ret / Event(id=aid, value=self.values[aid])
 
-            ret = ret / Event(value=self.values[aid])
-
-        return ret
+        return ret / EventEnd()
 
 
-event = EventBuilder()
-event.add(0, 230)
-event.add(1, 230)
+event = EventBuilderID()
+event.add(1, 4294967295)
 event.add(3, 30)
 
-
 print("Sending...")
-#hexdump()
-sendp(event.build())
+
+pkt = event.build() / "Payload"
+pkt.show()
+hexdump(pkt)
+sendp(pkt)
